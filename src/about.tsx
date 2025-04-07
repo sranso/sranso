8ea8905c385308485323a4b07e8cf91d271da15d
Sarah Ransohoff <sarah.ransohoff@clay.run>
2025-04-06 21:48:28 -0400

    add about

--- src/about.tsx
+++ src/about.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { Sidebar } from './components/Sidebar.tsx';
+import { Sidebar } from '../src/components/Sidebar.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Sidebar />
     <main className='flex-1 p-6 md:ml-64'>
-      <p>hi</p>
+      <p>about</p>
     </main>
   </StrictMode>
 );
