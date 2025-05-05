101cd8e92602b54af7501e2a02cbcc19c2d9c6ce
Sarah Ransohoff <sarah.ransohoff@clay.run>
2025-05-05 12:55:28 -0400

    fix: remove direct CSS link for production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
