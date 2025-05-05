f7d812b5b13ad6dc307b4e8568f797a21627acb5
Sarah Ransohoff <sarah.ransohoff@clay.run>
2025-05-05 13:14:22 -0400

    lil fixes

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,11 +3,7 @@ export function Home() {
     <main className='flex-1 p-6 md:ml-64'>
       <div className='w-full mx-auto'>
         <div className='h-[800px]'>
-          <img
-            className='h-full'
-            src='/mokuhanga/2025-04-05-xena.jpg'
-            alt='Xena the seal'
-          />
+          <img src='/mokuhanga/2025-04-05-xena.jpg' alt='Xena the seal' />
         </div>
       </div>
     </main>
