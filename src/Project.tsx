efa9ddd7ba0425582edd0b879259ff907ff24d9c
Sarah Ransohoff <sarah.ransohoff@clay.run>
2025-05-08 10:52:22 -0400

    add desc to shirts

--- src/Project.tsx
+++ src/Project.tsx
@@ -17,7 +17,9 @@ export function Project() {
 
   return (
     <main className='flex-1 p-6 md:ml-64'>
-      {/* <p>{project.description}</p> */}
+      {project.description && (
+        <div className='max-w-7xl mx-auto'>{project.description}</div>
+      )}
       <div className='max-w-7xl mx-auto'>
         <Gallery artworks={artworks} />
       </div>
